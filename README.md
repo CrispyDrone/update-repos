9ed988e45d9007e9720981f7108f0f26cf0323ad
CrispyDrone <mybluewaterfall@hotmail.com>
2019-08-25 17:27:39 +0200

    Added regex support for repository name filtering. Prevent script from terminating in case a remote ref is not found. Updated README.

--- README.md
+++ README.md
@@ -1,19 +1,34 @@
 # update-repos
-Small bash script to quickly update specified branches for all repositories in a root folder.
+Small bash script to quickly update repositories in a root folder. You can specify from which remotes content needs to be fetched, which branches need to be included, and which repositories need to be updated by using a regular expression (case insensitive). 
 
 ## Dependencies
 + git: <https://git-scm.com/>
 
 ## How to use
-+ Update all master and develop branches in all repositories contained in a folder "my-repos" using the only fast forward merge strategy:
+```
+update-repos.sh <folder> <remote> <branches> <ff-only> <regex>
+```
+
++ `<folder>`: The root folder
++ `<remote>`: The git remote you want to update
++ `<branches>`: A comma separated list of branches you want to update.
++ `<ff-only>`: `true` or `false` specifying whether to merge using fast-forward only strategy or not.
++ `<regex>`: A regular expression used to filter the repositories that should be updated, based on their name (i.e. the `.git` part of the clone url).
+
+### Examples
++ Update all master and develop branches using the origin remote in all repositories contained in a folder "my-repos" using only the fast forward merge strategy:
 
   ```
-  update-repos.sh my-repos true master,develop
+  update-repos.sh my-repos origin master,develop true
   ```
 
-+ Update all master branches in all repositories contained in a folder "my-repos" while allowing non fast foward merges:
++ Update all master branches in all repositories matched 'notes' contained in a folder "my-repos" while allowing non fast foward merges:
 
   ```
-  update-repos.sh my-repos false master
+  update-repos.sh my-repos origin master false notes
   ```
 
+## In the pipeline
++ Support for parallel processing
++ Improved command line experience with flags etc
++ Support for multiple origins (only content fetching, not merging...?)
--- update-repos.sh
+++ update-repos.sh
@@ -1,31 +1,96 @@
-# $1 = root directory, $2 = ff only, $3 = branch names
-set -e
-cd $1
+# $1 = root directory, $2 = remote, $3 = branch names, $4 = ff only, $5 = regex 
+# set -e
+
+# constants
+get_repository_name_regex='^.+\/\(.*\)\.git$'
+
+# variables
+root_directory="${1:-.}"
+remote="${2:-origin}"
+IFS=',' read -ra branches <<< "${3:-master}" 
+ff_only="${4:-true}"
+repository_name_regex="${5:-.*}"
+
+# verify variables
+# echo "${root_directory}"
+# echo "${remote}"
+# echo "${branches[@]}"
+# echo "${ff_only}"
+
+# functions
+elementIn()
+{
+	local toSearch element
+	toSearch="${1}"
+  	shift
+  	for element; do [[ "$element" == "$toSearch" ]] && return 0; done
+  	return 1
+}
+
+# script
+cd ${root_directory}
+echo "changed to directory ${root_directory}"
 
 for folder in */
 do
 	cd "${folder}"
-	echo "in folder $folder"
+	echo "changed to directory $folder"
 
-	remote_url=$(git remote get-url origin)
+	IFS='' read -ra existing_remotes <<< $(git remote)
+
+	echo "${existing_remotes[@]}"
+
+	if ! elementIn "${remote}" "${existing_remotes[@]}"
+	then
+		echo "${remote} does not exist!"
+		echo "Skipping this repository."
+		cd "${root_directory}"
+		continue;
+	fi
+
+	remote_url=$(git remote get-url "${remote}")
+	repository_name=$(echo "${remote_url}" | sed 's/'"${get_repository_name_regex}"'/\1/')
+
+	# echo "${remote_url}"
+	# echo "${repository_name}"
+
+	shopt -s nocasematch
+
+	if [[ ! ${repository_name} =~ ${repository_name_regex} ]]
+	then
+		echo "Skipping ${repository_name} since it does not match the given regex ${repository_name_regex}"
+		cd "${root_directory}"
+		continue;
+	fi
+
+	shopt -u nocasematch
+
 	echo 'fetching new content for '"${remote_url}"
+	git fetch --tags --prune "${remote_url}" "${branches[@]}"
 
-	git fetch --all --tags --prune
-
-	IFS=',' read -ra branches <<< "$3" 
+	if [[ ! $? -eq 0 ]]
+	then
+		echo "One or multiple branches of ${branches[@]} were not found on the remote."
+		echo "Skipping this repository."
+		cd "${root_directory}"
+		continue;
+	fi
 
 	for branch in "${branches[@]}"
 	do
 		echo "checking out branch $branch"
 		git checkout "${branch}"
 
-		if [[ "${2}" == 'true' ]]
+		if [[ "${ff_only}" = true ]]
 		then
-			echo "fast foward merging with origin/$branch"
-			git merge --ff-only origin/"${branch}"
+			echo "fast foward merging with ${remote}/${branch}"
+			git merge --ff-only "${remote}"/"${branch}"
 		else
-			echo "merging with origin/$branch"
-			git merge origin/"${branch}"
+			echo "merging with ${remote}/${branch}"
+			git merge "${remote}"/"${branch}"
 		fi
 	done
+
+	cd "${root_directory}"
 done
+
